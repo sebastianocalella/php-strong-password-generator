81140e79ab22ac24afc7057683c3d66b9815807c
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-26 16:39:28 +0100

    random password generator system

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-    $charactersList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","0","1","2","3","4","5","6","7","8","9",'.',',',':','?','=','-','(',')','"','\'','/','%','@','!'];
-    $char_num = $_GET["characters_num"]
+    $characters_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","0","1","2","3","4","5","6","7","8","9",'.',',',':','?','=','-','(',')','"','\'','/','%','@','!'];
+    $char_num = ($_GET["characters_num"]) ?? false;
 ?>
 
 <!DOCTYPE html>
@@ -12,17 +12,16 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="GET">
+    <form action="./index.php" method="GET">
         <input type="text" name="characters_num" placeholder="number of characters yo need">
         <button type="submit">generate password</button>
     </form>
     <?php
         function generatePassword($array,$psw_len){
-            $psw = implode(array_rand($array, $psw_len));
+            $psw = implode("",array_rand(array_flip($array),9));
             return $psw;
         }
-
-        echo "<p> {$generatePassword($charactersList,$char_num)} </p>"
+        var_dump(generatePassword($characters_list,5));
     ?>
     <h3>memory</h3>
 </body>
